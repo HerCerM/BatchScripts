c0a22fa3e2fe5e7e257668fbddcc56f7b18f7b40
HerCerM <42811720+HerCerM@users.noreply.github.com>
2019-05-28 20:41:06 -0500

    Output directory added as fifth argument.

--- ftra.bat
+++ ftra.bat
@@ -13,10 +13,7 @@ rem Get file name of argument
 for %%f in (%1) do set out=%%~nf
 
 rem Do mTarget.%4 mask with aMask.%4
-magick mTarget.%4 aMask.%4 -alpha off -compose copy_opacity -composite %out%.%4
+magick mTarget.%4 aMask.%4 -alpha off -compose copy_opacity -composite %5\%out%.%4
 
 rem Clean up
 del aMask.%4 & del mTarget.%4
-
-rem Wait 1 second for clean up
-ping -n 2 1.1.1.1 > nul
