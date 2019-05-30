fa843dd39f16f05b57279cb8054747ee62a741b5
HerCerM <42811720+HerCerM@users.noreply.github.com>
2019-05-30 15:26:01 -0500

    webP conversion now done with cwebp.

--- witra.bat
+++ witra.bat
@@ -1,19 +1,12 @@
 @echo off
 
-rem Get alpha mask
-magick %1 -resize %2\x%3 aMask.png
-magick aMask.png -alpha extract aMask.png
-magick aMask.png -gravity center -background black -extent %2\x%3 aMask.png
-
-rem Get image to mask
-magick %1 -resize %2\x%3 mTarget.png
-magick mTarget.png -gravity center -extent %2\x%3 mTarget.png
-
 rem Get file name of argument
 for %%f in (%1) do set out=%%~nf
 
-rem Do mTarget.png mask with aMask.png
-magick mTarget.png aMask.png -quality 50 -define webp:lossless=false -alpha off -compose copy_opacity -composite %4\%out%.webp
+rem Create webP file
+magick %1 -resize 512x512 %2\temp.png
+magick %2\temp.png -background none -gravity center -extent 512x512 %2\temp.png
+cwebp -q 50 %2\temp.png -o %2\%out%.webp
 
 rem Clean up
-del aMask.png & del mTarget.png
+del %2\temp.png
