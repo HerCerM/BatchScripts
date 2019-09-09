0d2513fdf49e2d975231e08655c99bdf5808581a
Hernán <hjcerveram@hotmail.com>
2019-09-09 08:35:08 -0500

    Remove date tag and delete removed files on archive update

--- backup.bat
+++ backup.bat
@@ -23,22 +23,17 @@ if %errorlevel%==1 exit /b 1
 :: Build name of backup file in the following format:
 :: 	Backup[<target-folder-name>] Date[<day> <month> <year>]
 
-:: Extract date from 'date /t' command
-for /f "tokens=2" %%a in ('date /t') do set today=%%a
-:: Extract day, month and year from 'date /t' command
-for /f "tokens=1,2,3 delims=/ " %%a in ("%today%") do ^
-set day=%%a& set month=%%b& set year=%%c
 :: Build name of backup file
 for /d %%a in (%targetFolder%) do set folderName=%%~na
-set archiveName=Backup[%folderName%] Date[%day%-%month%-%year%]
+set archiveName=Backup[%folderName%]
 
 :: Backup file path
 :: Update or create backup of target folder
-7z u -r -p%password% "%backupLocation%\%archiveName%" "%targetFolder%"
+7z u -p%password% "%backupLocation%\%archiveName%" -uq0 "%targetFolder%"
 exit /b 0
 
 :checkFileExistence
 :: Error message if a file is not found
-set fileNotFound=ERROR: File not found, backup aborted.
-if not exist %~1 echo %fileNotFound% & pause & exit /b 1
+set fileNotFound=[ERROR] Backup aborted, file not found:
+if not exist %~1 echo %fileNotFound% %~1 & pause & exit /b 1
 exit /b 0
